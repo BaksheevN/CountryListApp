e50d255e33d52fb63ad1c4555067724550b57631
Nikita Baksheev <nikiba@rarus.ru>
2025-06-19 11:36:39 +0300

    Initial Commit

---
+++ Walmart test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Walmart test
+//
+//  Created by Nikita Baksheev on 6/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
